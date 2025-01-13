b6fbaac28a9867646780de6848e2b08427c02e4b
SGBORTO <120338217+SGBORTO@users.noreply.github.com>
2025-01-13 10:53:20 +0100

    aggiunta inizio correzione

--- visualizza_test_completato.php
+++ visualizza_test_completato.php
@@ -84,14 +84,23 @@ $stmt->close();
 
     ?>
     </div>
-    <div class="btn-group ms-3" role="group" aria-label="navigation">
-        <a href=<?=($_SESSION['privilegi']==2)?("doc_test_menu.php?id=". $test_id ) : ("test.php?id=". $test_id)?> class="btn btn-outline-primary" >
-            Indietro 
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-return-left" viewBox="0 0 16 16">
-            <path fill-rule="evenodd" d="M14.5 1.5a.5.5 0 0 1 .5.5v4.8a2.5 2.5 0 0 1-2.5 2.5H2.707l3.347 3.346a.5.5 0 0 1-.708.708l-4.2-4.2a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 8.3H12.5A1.5 1.5 0 0 0 14 6.8V2a.5.5 0 0 1 .5-.5"/>
-            </svg>
-        </a>
-        <a href=<?=($_SESSION['privilegi']==2)?("my_tests.php"):("tests.php")?> class="btn btn-outline-primary" >Torna ai tuoi test</a>
+        <div class="d-flex">
+        <div class="btn-group ms-3" role="group" aria-label="navigation">
+            <a href=<?=($_SESSION['privilegi']==2)?("doc_test_menu.php?id=". $test_id ) : ("test.php?id=". $test_id)?> class="btn btn-outline-primary" >
+                Indietro 
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-return-left" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M14.5 1.5a.5.5 0 0 1 .5.5v4.8a2.5 2.5 0 0 1-2.5 2.5H2.707l3.347 3.346a.5.5 0 0 1-.708.708l-4.2-4.2a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 8.3H12.5A1.5 1.5 0 0 0 14 6.8V2a.5.5 0 0 1 .5-.5"/>
+                </svg>
+            </a>
+            <a href=<?=($_SESSION['privilegi']==2)?("my_tests.php"):("tests.php")?> class="btn btn-outline-primary" >Torna ai tuoi test</a>
+        </div>
+        <div class="ms-auto me-3">
+            <a href="<?= ($_SESSION['privilegi'] == 2) ? 'correggi_test.php?id=' . $_GET['id'] . '&studente=' . $test['studente'] : '#' ?>" 
+                class="btn btn-outline-primary <?= ($_SESSION['privilegi'] == 2) ? '' : 'disabled' ?>" 
+                aria-disabled="<?= ($_SESSION['privilegi'] == 2) ? 'false' : 'true' ?>">
+                Correggi test
+            </a>
+        </div>
     </div>
     <?php require("./include_bs_js.php"); ?>
 </body>
